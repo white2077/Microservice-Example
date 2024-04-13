a3bab69ca6266e908a69601216c9543c1d24b4e9
white2077 <dungbui8198@gmail.com>
2024-04-13 18:50:07 +0700

    resoucre Server Config

---
+++ product/src/main/java/com/white/product/core/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.white.product.core.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri ;
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+         http.oauth2ResourceServer(oauth2->oauth2.jwt().decoder(JwtDecoders.fromIssuerLocation(issuerUri)));
+         http.authorizeHttpRequests(auth ->{
+             try {
+                 auth.anyRequest().hasAuthority("ADMIN");
+//                            .and()
+//                            .oauth2ResourceServer()
+//                            .jwt()
+//                            .jwtAuthenticationConverter(jwtAuthenticationConverter());
+             } catch (Exception e) {
+                 throw new RuntimeException(e);
+             }
+         });
+         return http.build();
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter()  {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+            if (authorities != null) {
+                return authorities.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+            } else {
+                return Collections.emptyList();
+            }
+        });
+        return converter;
+    }
+}
